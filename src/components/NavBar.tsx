393122899c4a86800abc6117e5cf49fc80f58c4d
hamed hamze <hamed.hamze2212@gmail.com>
2025-02-23 15:54:18 +0330

    Build search input.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,15 @@ import { HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 import logoWhite from "../assets/Logo-white.png";
 import logoBlack from "../assets/Logo-black.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
     const { colorMode } = useColorMode();
 
     return (
-        <HStack justifyContent="space-between" padding="10px">
+        <HStack padding="10px">
             <Image src={colorMode === "dark" ? logoWhite : logoBlack} w="auto" h="80px" />
+            <SearchInput />
             <ColorModeSwitch />
         </HStack>
     );
