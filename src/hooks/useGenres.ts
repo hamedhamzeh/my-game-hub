49e9e5afe93d3ef46ee5a37b288bd5e6a8827dd2
hamed hamze <hamed.hamze2212@gmail.com>
2025-02-27 18:31:08 +0330

    Implement infinite queries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -14,8 +14,8 @@ const useGenres = () =>
     useQuery({
         queryKey: ["genres"],
         queryFn: apiClient.getAll,
-        staleTime: 24 * 60 * 60 * 1000, //24h
-        initialData: { count: genres.length, results: genres }
+        staleTime: 24 * 60 * 60 * 1000 //24h
+        // initialData: { count: genres.length, results: genres }
     });
 
 export default useGenres;
